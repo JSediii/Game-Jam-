7d8d86d4ee0a7102208d3d534c18d4ee8c179c08
lusho <purisimajethro@gmail.com>
2023-10-22 07:41:17 +0800

    Fixed collision between player and enemy

---
+++ Assets/Scripts/EnemyDestroy.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDestroy : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+}
+
+
+
+    
+
