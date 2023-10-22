a3c18eb69f9981ba95a26f750437ab2c12c71c06
Julius Sedee Castro <sedeejulcastro@gmail.com>
2023-10-22 17:05:51 +0800

    New Spawn

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -6,22 +6,33 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private float spawnRate = 1f;
     [SerializeField] private GameObject[] enemyPrefabs;
-    
-    private void Start(){
 
+    public int limitSpawn = 3;
+    private List<GameObject> spawnedEnemies = new List<GameObject>();
+
+    private void Start()
+    {
         StartCoroutine(Spawner());
     }
 
-    private IEnumerator Spawner (){
-
+    private IEnumerator Spawner()
+    {
         WaitForSeconds wait = new WaitForSeconds(spawnRate);
 
-        while(true)
+        while (true)
         {
             yield return wait;
-            int rand = Random.Range(0, enemyPrefabs.Length - 1);
-            GameObject enemyToSpawn = enemyPrefabs[rand];
-            Instantiate(enemyToSpawn, transform.position, Quaternion.identity);
+
+            // Remove destroyed enemies from the list
+            spawnedEnemies.RemoveAll(enemy => enemy == null);
+
+            if (spawnedEnemies.Count < limitSpawn)
+            {
+                int rand = Random.Range(0, enemyPrefabs.Length);
+                GameObject enemyToSpawn = enemyPrefabs[rand];
+                GameObject spawnedEnemy = Instantiate(enemyToSpawn, transform.position, Quaternion.identity);
+                spawnedEnemies.Add(spawnedEnemy);
+            }
         }
     }
 }
