660ad47206dc2960f761643f5b0677030cd25941
Julius Sedee Castro <sedeejulcastro@gmail.com>
2023-10-22 14:06:15 +0800

    Sample

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -5,11 +5,8 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private float spawnRate = 1f;
-
     [SerializeField] private GameObject[] enemyPrefabs;
-
     [SerializeField] private bool canSpawn = true;
-           
     
     private void Start(){
 
@@ -25,8 +22,6 @@ public class EnemySpawner : MonoBehaviour
             yield return wait;
             int rand = Random.Range(0, enemyPrefabs.Length - 1);
             GameObject enemyToSpawn = enemyPrefabs[rand];
-
-
             Instantiate(enemyToSpawn, transform.position, Quaternion.identity);
         }
     }
